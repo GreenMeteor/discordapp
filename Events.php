2593d5cc7aa71dbbf2f94b2f292ec3072a3404f7
WebCrew <palareas@gmail.com>
2017-01-16 13:26:41 +0100

    updated A.H.

--- Events.php
+++ Events.php
@@ -3,7 +3,7 @@ namespace humhub\modules\discordapp;
 
 use Yii;
 use yii\helpers\Url;
-use humhub\modules\discordapp\widgets\AdFrame;
+use humhub\modules\discordapp\widgets\DiscordappFrame;
 use humhub\models\Setting;
 
 class Events extends \yii\base\Object
@@ -15,19 +15,19 @@ class Events extends \yii\base\Object
             'label' => Yii::t('DiscordappModule.base', 'Discordapp Settings'),
             'url' => Url::toRoute('/discordapp/admin/index'),
             'group' => 'settings',
-            'icon' => '<i class="fa fa-discord"></i>',
-            'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'discordapp' && Yii::$app->controller->id == 'admin',
+            'icon' => '<i class="fa fa-weixin"></i>',
+            'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'discord' && Yii::$app->controller->id == 'admin',
             'sortOrder' => 650
         ]);
     }
 
-public static function addChatFrame($event)
+public static function addDiscordappFrame($event)
     {
         if (Yii::$app->user->isGuest) {
             return;
         }
         $event->sender->view->registerAssetBundle(Assets::className());
-        $event->sender->addWidget(ChatFrame::className(), [], [
+        $event->sender->addWidget(DiscordappFrame::className(), [], [
             'sortOrder' => Setting::Get('timeout', 'discordapp')
         ]);
     }
