2cb7abd816688ba0bf200ad48ab76b900863c307
WebCrew <palareas@gmail.com>
2017-01-16 13:30:44 +0100

    updated A.H.
    line 11 has to be replaced with Fellis code

---
+++ widgets/views/discordappframe.php
@@ -0,0 +1,14 @@
+<?php
+use yii\helpers\Url;
+use humhub\models\Setting;
+?>
+<div class="panel">
+  <div class="panel-heading">
+    <?=Yii::t('DiscordappModule.base', '<strong>Discord</strong> App'); ?>
+  </div>
+  <div class="panel-body">
+      
+<iframe src="https://discordapp.com/widget?id=218530518143205376&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0"></iframe>
+
+</div>
+</div>
