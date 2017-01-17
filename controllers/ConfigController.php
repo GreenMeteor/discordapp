4908bcda986b0721dd99025ea984cff2c188eec6
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-01-16 19:17:14 -0500

    Update ConfigController.php

--- controllers/ConfigController.php
+++ controllers/ConfigController.php
@@ -1,19 +1,27 @@
 <?php
-namespace humhub\modules\discord\controllers;
+
+namespace humhub\modules\discordapp\controllers;
+
 use Yii;
-use humhub\modules\notes\models\ConfigureForm;
+use yii\helpers\Url;
+use humhub\modules\admin\components\Controller;
 use humhub\models\Setting;
-class ConfigController extends \humhub\modules\admin\components\Controller
+use humhub\modules\discordapp\models\EditForm;
+
+class AdminController extends Controller
 {
+    /**
+     * Configuration Action for Super Admins
+     */
     public function actionIndex()
     {
-        $form = new ConfigureForm();
-        $form->baseUrl = Setting::Get('baseUrl', 'discord');
-        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
-            $form->baseUrl = Setting::Set('baseUrl', $form->baseUrl, 'discord');
-            return $this->redirect(['/discord/config']);
+        $form = new EditForm();
+        if ($form->load(Yii::$app->request->post()) && $form->validate() && $form->save()) {
+            return $this->redirect(Url::to(['/discordapp/admin/index']));
         }
-        return $this->render('index', array('model' => $form));
+        
+        return $this->render('index', ['model' => $form]);
     }
 }
+
 ?>
