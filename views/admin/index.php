6043de39a3dbda03882922ca8904eb352492db46
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-01-31 23:42:14 -0500

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,25 +1,25 @@
 <?php
 
-use humhub\compat\CActiveForm;
-use humhub\compat\CHtml;
+use humhub\compat\Html;
+use humhub\compat\ActiveForm;
 use humhub\models\Setting;
-use humhub\modules\discord\controllers\AdminController;
+use humhub\modules\discordapp\controllers\AdminController;
 
 ?>
 
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('DiscordModule.base', '<strong>Discord</strong>'); ?></div>
+	<div class="panel-heading"><?php echo Yii::t('DiscordappModule.base', '<strong>Discord</strong>'); ?></div>
 	<div class="panel-body">
-		<?php $form = CActiveForm::begin(['id' => 'discord-settings-form']); ?>
-			<?=$form->errorSummary($model); ?>
-			<p class="help-block"><?=Yii::t('DiscordModule.base', 'Server ID:  "000000000000000000"'); ?></p>
+		<?php $form = ActiveForm::begin(['id' => 'discord-settings-form']); ?>
+			<?= $form->errorSummary($model); ?>
+			<p class="help-block"><?php echo Yii::t('DiscordappModule.base', 'Server ID:  "000000000000000000"'); ?></p>
 			<div class="form-group">
-			        <?=$form->labelEx($model, 'sort'); ?>
-				<?=$form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'discord')]); ?>
+			        <?php echo $form->labelEx($model, 'number'); ?>
+				<?php echo $form->textField($model, 'number', ['class' => 'form-control', 'readonly' => Setting::IsFixed('number', 'discordapp')]); ?>
 			</div>
-			<p class="help-block"><?=Yii::t('DiscordModule.base', 'Server ID.'); ?></p>
-			<?= CHtml::submitButton(Yii::t('DiscordModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
-			<?=\humhub\widgets\DataSaved::widget(); ?>
-		<?php CActiveForm::end(); ?>
+			<p class="help-block"><?[php echo Yii::t('DiscordappModule.base', 'Server ID.'); ?></p>
+			<?php echo Html::submitButton(Yii::t('DiscordappModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
+			<?php echo \humhub\widgets\DataSaved::widget(); ?>
+		<?php ActiveForm::end(); ?>
 	</div>
 </div>
