13fa47bbdf7642c9bcea53fb8b1c6a80233b6bb1
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-02-15 12:56:47 -0500

    Update discordappframe.php

--- widgets/views/discordappframe.php
+++ widgets/views/discordappframe.php
@@ -1,14 +1,16 @@
 <?php
+
 use yii\helpers\Url;
 use humhub\models\Setting;
+
 ?>
 <div class="panel">
   <div class="panel-heading">
-    <?=Yii::t('DiscordappModule.base', '<strong>Discord</strong> App'); ?>
+    <?php echo Yii::t('DiscordappModule.base', '<strong>Discord</strong> App'); ?>
   </div>
   <div class="panel-body">
       
-<iframe src="<?php Url::to(['https://discordapp.com/widget?id=', 'id' => Url::to($this->context->route)]); ?>">
+<iframe src="<?php echo Url::to(['https://discordapp.com/widget?id=', 'id' => Url::to($this->context->route)]); ?>">
     <?php $this->render($view); ?>
 </iframe>
     
