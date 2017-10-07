5d4c92e62c1ee42e770176716e403be5de3de293
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-08 02:55:17 +0900

    Create base.php

---
+++ messages/zh_cn/base.php
@@ -0,0 +1,8 @@
+<?php
+	return [
+		'<strong>Discord</strong> Chat' => '',
+		'Discord Settings' => '',
+		'Discord Widget URL:' => '',
+		'<strong>Discordapp</strong> module configuration' => '',
+		'save' => '',
+	];
