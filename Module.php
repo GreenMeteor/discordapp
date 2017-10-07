976ef9b8ca3f5f2cbdd37a87e62e6f04215935ca
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-07 11:53:39 +0900

    Update Module.php

--- Module.php
+++ Module.php
@@ -2,19 +2,27 @@
 
 namespace humhub\modules\discordapp;
 
+use Yii;
+use yii\helpers\Url;
+
 class Module extends \humhub\components\Module
 {
 
     /**
      * @inheritdoc
      */
+    public function getConfigUrl()
+    {
+        return Url::to([
+                    '/discordapp/admin'
+        ]);
+    }
     public function getServerUrl()
     {
         $url = $this->settings->get('serverUrl');
         if (empty($url)) {
-            return 'https://discordapp.com/widget?id=';
+            return 'https://discordapp.com';
         }
-
         return $url;
     }
 
