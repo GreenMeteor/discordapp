f1a410bb7006e9ac1148ed93e0f9cd2f149a5d7a
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-07 10:13:19 +0900

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -3,24 +3,22 @@
 namespace humhub\modules\discordapp\controllers;
 
 use Yii;
-use yii\helpers\Url;
-use humhub\models\Setting;
 use humhub\modules\admin\components\Controller;
 use humhub\modules\discordapp\models\ConfigureForm;
 
 class AdminController extends Controller
 {
-    /**
-     * Configuration Action for Super Admins
-     */
+
     public function actionIndex()
     {
-        $form = new ConfigureForm();
-        if ($form->load(Yii::$app->request->post()) && $form->validate() && $form->save()) {
-            return $this->redirect(Url::to(['/discordapp/admin/index']));
+        $model = new ConfigureForm();
+        $model->loadSettings();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $this->view->saved();
         }
-        
-        return $this->render('index', ['model' => $form]);
+
+        return $this->render('index', ['model' => $model]);
     }
+
 }
-?>
