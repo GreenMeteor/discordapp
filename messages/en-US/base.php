e46b466f9d28ce3e7591cff89b717c23478d1215
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-08 03:03:41 +0900

    Update base.php

--- messages/en-US/base.php
+++ messages/en-US/base.php
@@ -4,6 +4,6 @@
 		'<strong>Discord</strong> Chat' => '<strong>Discord</strong> Chat',
 		'Discord Settings' => 'Discord Settings',
 		'Discord Widget URL:' => 'Discord Widget URL:',
-		'<strong>Discordapp</strong> module configuration' => '<strong>Discordapp</strong> module configuration',
+		'<strong>Discord</strong> module configuration' => '<strong>Discord</strong> module configuration',
 		'save' => 'Save',
 	];
