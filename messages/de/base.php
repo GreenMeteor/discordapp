58ae356029a4e4d52f2c7f8ec4f01a6c85325b28
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-13 09:47:04 +0900

    Update base.php

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,8 +1,8 @@
 <?php
 	return [
-		'<strong>Discord</strong> Chat' => '',
-		'Discord Settings' => '',
-		'Discord Widget URL:' => '',
-		'<strong>Discord</strong> module configuration' => '',
+		'<strong>Discord</strong> Chat' => '<strong>Discord</strong> Chat',
+		'Discord Settings' => 'Discord Einstellungen',
+		'Discord Widget URL:' => 'Discord Widget URL:',
+		'<strong>Discord</strong> module configuration' => '<strong>Discord</ strong> Modul konfiguration',
 		'Save' => '',
 	];
