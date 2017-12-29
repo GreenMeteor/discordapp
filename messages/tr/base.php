513e870b2fa3b1d5b40014c8d092b4259f8fb90f
Çağatay <cagatayldzz@gmail.com>
2017-12-29 13:11:34 +0300

    tr Added

---
+++ messages/tr/base.php
@@ -0,0 +1,9 @@
+<?php
+
+	return [
+		'<strong>Discord</strong> Chat' => '<strong>Discord</strong> Sohbet',
+		'Discord Settings' => 'Discord Ayarlar',
+		'Discord Widget URL:' => 'Discord Widget URL:',
+		'<strong>Discord</strong> module configuration' => '<strong>Discord</strong> modül yapılandırması',
+		'Save' => 'Kaydet',
+	];
