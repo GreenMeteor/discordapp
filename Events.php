59bdd388af83743b7927bfdae9bfe173e488a4e2
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-08-30 21:39:37 +0900

    Chg: className() to class

--- Events.php
+++ Events.php
@@ -5,9 +5,10 @@ namespace humhub\modules\discordapp;
 use Yii;
 use yii\helpers\Url;
 use humhub\modules\discordapp\widgets\DiscordappFrame;
+use yii\base\BaseObject;
 use humhub\models\Setting;
 
-class Events extends \yii\base\BaseObject
+class Events extends BaseObject
 {
 
     public static function onAdminMenuInit(\yii\base\Event $event)
@@ -27,8 +28,8 @@ public static function addDiscordappFrame($event)
         if (Yii::$app->user->isGuest) {
             return;
         }
-        $event->sender->view->registerAssetBundle(Assets::className());
-        $event->sender->addWidget(DiscordappFrame::className(), [], [
+        $event->sender->view->registerAssetBundle(Assets::class);
+        $event->sender->addWidget(DiscordappFrame::class, [], [
             'sortOrder' => Setting::Get('timeout', 'discordapp')
         ]);
     }
