225aa7eef35d82618d40feae2d797ed10f5568dd
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-06-03 15:40:35 -0400

    Update SpaceAdminController.php

--- controllers/SpaceAdminController.php
+++ controllers/SpaceAdminController.php
@@ -1,1 +1,25 @@
 
+<?php
+
+namespace humhub\modules\discordapp\controllers;
+
+use Yii;
+use humhub\modules\space\controllers\SpaceController;
+use humhub\modules\discordspace\models\ConfigureForm;
+
+class SpaceAdminController extends SpaceController
+{
+
+    public function actionIndex()
+    {
+        $model = new ConfigureForm();
+        $model->loadSettings();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $this->view->saved();
+        }
+
+        return $this->render('index', ['model' => $model]);
+    }
+
+}
