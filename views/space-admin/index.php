ee54d00202241c9bb6fcf5a8e6aa6fcabd8c3ef5
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-08-07 16:50:40 -0400

    Update index.php

--- views/space-admin/index.php
+++ views/space-admin/index.php
@@ -1,1 +1,24 @@
-TBA
+<?php
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+?>
+
+<div class="panel panel-default">
+
+    <div class="panel-heading"><?= Yii::t('DiscordappModule.base', '<strong>Discord</strong> module configuration'); ?></div>
+
+    <div class="panel-body">
+
+        <?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
+        <div class="form-group">
+            <?= $form->field($model, 'sUrl'); ?>
+        </div>
+
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('DiscordappModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
