6b454ea1c44d4d504fc08cea9e1bf8bc7e76e5e7
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-08-07 20:13:55 -0400

    Update DiscordappFixture.php

--- tests/codeception/fixtures/DiscordappFixture.php
+++ tests/codeception/fixtures/DiscordappFixture.php
@@ -5,15 +5,7 @@ namespace humhub\modules\discordapp\tests\codeception\fixtures;
 use humhub\modules\discordapp\models\ConfigureForm;
 use yii\test\ActiveFixture;
 
-class TaskFixture extends ActiveFixture
+class DiscordappFixture extends ActiveFixture
 {
-    public $modelClass = Task::class;
-    public $dataFile = '@discordapp/tests/codeception/fixtures/data/task.php';
-    
-     public $depends = [
-        DiscordappItemFixture::class,
-        DiscordappSettingFixture::class,
-        DiscordappReminderFixture::class,
-        DiscordappUserFixture::class,
-    ];
+
 }
