a76d84399bd5828f902bef2db16877b3f764a234
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-08-07 20:48:02 -0400

    Update setup-humhub.sh

--- .travis/setup-humhub.sh
+++ .travis/setup-humhub.sh
@@ -12,14 +12,12 @@ git clone --branch ${HUMHUB_VERSION} --depth 1 https://github.com/humhub/humhub.
 composer install --prefer-dist --no-interaction
 
 npm install
-
-if [ "${HUMHUB_VERSION}" != 'v1.3.14' ] ; then
-  grunt build-assets
-fi
+grunt build-assets
 
 cd ${HUMHUB_PATH}/protected/humhub/tests
 
 sed -i -e "s|'installed' => true,|'installed' => true,\n\t'moduleAutoloadPaths' => ['$(dirname $old)']|g" config/common.php
+#cat config/common.php
 
 mysql -e 'CREATE DATABASE humhub_test;'
 php codeception/bin/yii migrate/up --includeModuleMigrations=1 --interactive=0
