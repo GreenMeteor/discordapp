2ceb88037833ba695764729653c5e184d1fbc5a5
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-03-31 10:25:59 -0400

    Create base.php

---
+++ messages/in_ID/base.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        '<strong>Discord</strong> Chat' => '<strong>Discord</strong> Obrolan',
+        'Discord Settings' => 'Discord Pengaturan',
+        'Discord Widget URL:' => 'URL Widget Discord:',
+        '<strong>Discord</strong> module configuration' => '<strong>Discord</strong> konfigurasi modul',
+        'Save' => 'Menyimpan',
+    ];
