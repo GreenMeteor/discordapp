0895b97b4e2f3ecca58ed3b780c583d4d00814a4
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-05-08 22:16:01 -0400

    Fix: Dup code

--- Module.php
+++ Module.php
@@ -10,18 +10,6 @@ class Module extends BaseModule
 {
 
     public $resourcesPath = 'resources';
-<?php
-
-namespace humhub\modules\discordapp;
-
-use Yii;
-use yii\helpers\Url;
-use humhub\components\Module as BaseModule;
-
-class Module extends BaseModule
-{
-
-    public $resourcesPath = 'resources';
 
     /**
      * @inheritdoc
@@ -31,24 +19,6 @@ class Module extends BaseModule
         return Url::to(['/discordapp/admin']);
     }
 
-    public function getServerUrl()
-    {
-        $url = $this->settings->get('serverUrl');
-        if (empty($url)) {
-            return 'https://discordapp.com' || 'https://discord.com';
-        }
-        return $url;
-    }
-
-}
-
-    /**
-     * @inheritdoc
-     */
-    public function getConfigUrl()
-    {
-        return Url::to(['/discordapp/admin']);
-    }
     public function getServerUrl()
     {
         $url = $this->settings->get('serverUrl');
