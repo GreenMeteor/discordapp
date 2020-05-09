15ff9731f7762f558a18ef42ae83659ea65d9313
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-05-08 20:53:51 -0400

    Enh: Implement #37

--- Module.php
+++ Module.php
@@ -4,8 +4,21 @@ namespace humhub\modules\discordapp;
 
 use Yii;
 use yii\helpers\Url;
+use humhub\components\Module as BaseModule;
 
-class Module extends \humhub\components\Module
+class Module extends BaseModule
+{
+
+    public $resourcesPath = 'resources';
+<?php
+
+namespace humhub\modules\discordapp;
+
+use Yii;
+use yii\helpers\Url;
+use humhub\components\Module as BaseModule;
+
+class Module extends BaseModule
 {
 
     public $resourcesPath = 'resources';
@@ -22,7 +35,25 @@ class Module extends \humhub\components\Module
     {
         $url = $this->settings->get('serverUrl');
         if (empty($url)) {
-            return 'https://discordapp.com';
+            return 'https://discordapp.com' || 'https://discord.com';
+        }
+        return $url;
+    }
+
+}
+
+    /**
+     * @inheritdoc
+     */
+    public function getConfigUrl()
+    {
+        return Url::to(['/discordapp/admin']);
+    }
+    public function getServerUrl()
+    {
+        $url = $this->settings->get('serverUrl');
+        if (empty($url)) {
+            return 'https://discordapp.com' || 'https://discord.com';
         }
         return $url;
     }
