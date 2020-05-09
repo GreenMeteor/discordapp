b87df2ee87165f1e84ee44ca97176f6bbe591e81
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-05-08 22:14:24 -0400

    Update Assets.php

--- Assets.php
+++ Assets.php
@@ -8,10 +8,6 @@ use yii\web\AssetBundle;
 class Assets extends AssetBundle
 {
 
-    public $publishOptions = [
-        'forceCopy' => true
-    ];
-
     public function init()
     {
         $this->sourcePath = dirname(__FILE__) . '/resources';
